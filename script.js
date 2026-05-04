4b8c42d2ded5f5398a4a634bf5202b3a924f3809
Torrewalk828 <166555048+Torrewalk828@users.noreply.github.com>
2026-05-03 22:38:19 -0400

    Implement quiz submission and reset functions
    Added functions to handle quiz submission and reset.

---
+++ script.js
@@ -0,0 +1,9 @@
+// External JavaScript file for quiz interaction
+
+function submitQuiz() {
+    alert("Thank you for completing the Web Technologies Quiz!");
+}
+
+function resetQuiz() {
+    alert("Your quiz has been reset.");
+}
