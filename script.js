ca9d0339cecd4a65dfa49e4822d81adff25aa598
Torrewalk828 <166555048+Torrewalk828@users.noreply.github.com>
2026-05-03 22:40:09 -0400

    Add footer with course information to script.js

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function submitQuiz() {
 function resetQuiz() {
     alert("Your quiz has been reset.");
 }
+
+<footer>
+<p>This is a class project for IT3203</p>
+<a href="https://ksuweb.github.io/IT3203 Website/">Course Website</a>
+</footer>
